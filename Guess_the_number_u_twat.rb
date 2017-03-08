dd7e61efd1d9aa0b866056151694b5e82f0eb55a
itggot-liam-pagrell <liam.pagrell@itggot.se>
2017-03-08 11:02:04 +0100

    Guess the number initial commit
    poop

---
+++ Guess_the_number_u_twat.rb
@@ -0,0 +1,20 @@
+def guess_the_number_u_twat
+
+  poo = rand(1..10)
+  puts poo
+  svar = 0
+  poopie = 0
+
+  while poopie == 0
+    svar = gets.chomp.to_i
+    if poo == svar
+      puts "Gj u Twat"
+      poopie = 1
+    else
+      puts "U BLING DISHONOL TO FAMIRU!!1!1!!!11"
+    end
+  end
+
+end
+
+guess_the_number_u_twat
